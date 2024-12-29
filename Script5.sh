3853fd5eb181a98ab3a1cb2b3b3c2bdbacf22298
dhananjay1611 <148035392+dhananjay1611@users.noreply.github.com>
2024-12-29 14:58:14 +0530

    Add files via upload

---
+++ Script5.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#This script is to make variable read only, means we cannot set the value of NAME variable again
+NAME=Young-Minds
+readonly NAME
+NAME=DEVOPS
+echo "my name is: $NAME"
+echo "my name is: dhananjay"
